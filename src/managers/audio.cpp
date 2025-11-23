99b8caeec4050fea0768451c121706b994825873
Daniel Biasiotto <daniel.biasiotto@edu.unito.it>
2025-11-23 23:10:25 +0100

    fix: Header Include and printf Fixes
    Corrected header include paths in 'src/managers/input.cpp' and 'src/managers/audio.cpp' from local to managers-prefixed paths (e.g., '#include "input.hpp"' to '#include "managers/input.hpp"'). Additionally, added '#include <cstdio>' to 'src/managers/sprite.cpp' to resolve an undeclared 'printf' function error. These changes address compilation errors encountered due to incorrect header references.

---
+++ src/managers/audio.cpp
@@ -0,0 +1,30 @@
+#include "managers/audio.hpp"
+#include <cstdio>
+#include <raylib.h>
+
+const char *OST_FILE = "../assets/ost/ost1.ogg";
+Sound _ost;
+
+bool initializeAudio() {
+    InitAudioDevice();
+    SetMasterVolume(100);
+    return IsAudioDeviceReady();
+}
+
+void loadOST() {
+    _ost = LoadSound(OST_FILE);
+
+    if (IsSoundValid(_ost)) {
+        printf("Music stream is valid\n");
+    } else {
+        printf("Music stream is not valid\n");
+        return;
+    }
+}
+
+void playOST() { PlaySound(_ost); }
+
+void closeAudio() {
+    UnloadSound(_ost);
+    CloseAudioDevice();
+}
---
+++ src/managers/sprite.cpp
@@ -0,0 +1,61 @@
+#include "managers/sprite.hpp"
+#include <cstdio>
+#include <raylib.h>
+
+// RLAPI Texture2D LoadTexture(const char *fileName);
+// RLAPI bool IsTextureValid(Texture2D texture);
+// RLAPI void UnloadRenderTexture(RenderTexture2D target);
+// RLAPI void DrawTextureV(Texture2D texture, Vector2 position, Color tint);
+// RLAPI Image ImageFromImage(Image image, Rectangle rec);
+// RLAPI void DrawTextureRec(Texture2D texture, Rectangle source, Vector2
+// position, Color tint);
+
+// NOTE: spritesheet is 16x16
+const char *_SPRITESHEET =
+    "../../assets/sprites/dungeonmode/bitmap/dungeon-mode.png";
+const int _SPRITESHEET_COLS = 16;
+const int _SPRITESHEET_ROWS = 16;
+Texture2D _spritesheet;
+float _sprite_width, _sprite_height;
+
+struct Sprite {
+    Vector2 position;
+    float rotation;
+    Rectangle size;
+    Vector2 ID;            // this is the position in the spritesheet
+    Rectangle sprite_rect; // actual sprite position
+    Color tint;
+};
+
+Sprite _player_sprite = {
+    .position = {0, 0},
+    .rotation = 0,
+    .size = {0, 0, 32, 32},
+    .ID = {14, 14},
+    .sprite_rect = {0, 0, 0, 0},
+    .tint = WHITE,
+};
+
+void initializeSpritesheet() {
+    _spritesheet = LoadTexture(_SPRITESHEET);
+    if (IsTextureValid(_spritesheet)) {
+        printf("spritesheet is valid\n");
+    } else {
+        printf("spritesheet is not valid\n");
+        return;
+    }
+    _sprite_width = _spritesheet.width / _SPRITESHEET_COLS;
+    _sprite_height = _spritesheet.height / _SPRITESHEET_ROWS;
+    _player_sprite.sprite_rect = {_player_sprite.ID.x, _player_sprite.ID.y,
+                                  _sprite_width, _sprite_height};
+}
+
+void drawPlayer(Vector2 position) {
+    _player_sprite.position = position;
+
+    DrawTexturePro(_spritesheet, _player_sprite.sprite_rect,
+                   _player_sprite.size, position, _player_sprite.rotation,
+                   _player_sprite.tint);
+}
+
+void unloadSpritesheet() { UnloadTexture(_spritesheet); }
